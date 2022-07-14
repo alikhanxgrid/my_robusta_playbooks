9a80633e46916e51a8df59ced17d816f79aea031
Muhammad Ali Khan <ali.khan@xgrid.co>
2022-07-14 12:26:04 +0500

    Updated code

--- custom_playbooks/get_pod_metrics.py
+++ custom_playbooks/get_pod_metrics.py
@@ -9,9 +9,12 @@ def get_pod_metrics(pod_event: PodEvent):
         "pod": pod_event.get_pod().metadata.name,
         "namespace": pod_event.get_pod().metadata.namespace,
     }
-    metrics_enrichment = prom_query_runner.create_metrics_enrichment(
-        item_type=ResourceChartItemType.Pod,
-        labels=labels
-    )
-    pod_event.add_enrichment(metrics_enrichment)
+    #metrics_enrichment = prom_query_runner.create_metrics_enrichment(
+    #    item_type=ResourceChartItemType.Pod,
+    #    labels=labels
+    #)
+    block_list: List[BaseBlock] = []
+    #block_list.append(MarkdownBlock(f"Command results for *{params.bash_command}:*"))
+    block_list.append(MarkdownBlock(f"Test"))
+    pod_event.add_enrichment(block_list)
 
